f6dde18aa75aa16a038d345ac0da57e3fd178a0c
Pedrolcb <87148319+Pedrolcb@users.noreply.github.com>
2021-12-17 19:39:37 -0300

    Add files via upload

---
+++ resources/views/perfilpessoal.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil Pessoal</title>
+    <link rel="stylesheet" href="css/perfilpessoal.css">
+    <link href="https://fonts.googleapis.com/css2?family=Changa+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header> 
+      <h1 class="h3">Camarada absconso</h1>
+      <div class="menu col-1">
+        <div class="dropdown">
+            <button class="dropbtn">MENU</button>
+            <div class="dropdown-content">
+                <a href="/" style="color: white; text-decoration: none" >INICIO</a>
+                <a href="/Cadastrar" style="color: white; text-decoration: none;">CADASTRAR SALA</a>
+                <a href="/Perfil" style="color: white; text-decoration: none">PERFIL</a>
+                <a href="/Admsala" style="color: white; text-decoration: none">ADMINISTRAR SALA</a>
+                <br>
+                <a href="/Excluirsala" style="color: white; text-decoration: none">EXCLUIR SALA</a>
+                <br>
+                <a href="/Grupos" style="color: white; text-decoration: none"> GRUPOS</a>
+            </div>
+        </div>
+    </div>
+    </header> 
+    <section>
+      
+       <div class="info">
+           <h3>E-mail:  Richar.N.P.9000@gmail.com</h3>
+           <h3 >Nome: Richard Naves</h3>
+           <h3>Descrição:</h3>
+       </div>
+        
+       
+       <div>
+           <h3>Sou uma pessoa muito educada e talentosa, mas possuo alguns segredos...<br>
+            Líder administrativo do departamento de publicidade;</h3>
+       </div>
+       
+       <button class="button">Editar</button><button class="button">Salvar</button>
+       <table>  
+        <tr>
+            <th>Nome</th>
+            <th>Link</th>
+            <th>Data</th>
+            <th>Função</th>
+        </tr>
+        <tr>
+            <td>abc de minas</td>
+            <td>https://kclsdnavcdsakc</td>
+            <td>23-08-21</td>
+            <td>Organizador</td>
+        </tr>
+        <tr>
+            <td>fgh do rio</td>
+            <td>https://mlcmdspojsnm</td>
+            <td>18-08-21</td>
+            <td>Participante</td>
+        </tr>
+        <tr>
+            <td>jkl da bahia</td>
+            <td>https://cmnksdkafpew</td>
+            <td>08-01-21</td>
+            <td>Participante</td>
+        </tr>
+       </table>
+       <button class="button_sala">Criar nova sala</button>
+    </section>
+    <footer> 
+
+    </footer>
+</body>
+</html>
