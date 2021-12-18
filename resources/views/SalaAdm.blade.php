455f657c13b539e637dcf3e9a5fa00a18838f973
Pedrolcb <87148319+Pedrolcb@users.noreply.github.com>
2021-12-17 21:15:41 -0300

    Add files via upload

---
+++ resources/views/SalaAdm.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sala</title>
+    <link href="css/salaadm.css" rel="stylesheet">
+</head>
+<body>
+    <header> 
+      <h1 class="h3">Camarada absconso</h1>
+      <h2>menu</h2><br>
+    </header>
+    <nav>
+       <h1>Sala: abc de minas</h1>
+       <div class="data">
+           <h3>Data do evento: </h3> 
+       </div> <br>
+       <div class="maximo">
+          <h3 >Valor máximo:</h3>
+       </div><br>
+       <div class="minimo">
+          <h3>Valor mínimo:</h3>
+       </div>
+    </nav>
+    <section>
+        <table>  
+            <tr>
+                <th>Esperando o Organizador iniciar o sorteio...</th>
+            </tr>
+            <tr>
+                <td>Embaralhando os nomes em 3, 2, 1...</td>
+            </tr>
+            <tr>
+                <td>Sorteando os nomes...</td>
+            </tr>
+            <tr>
+                <td>Você sorteou o (a): Edivaldo Rodrigues</td>
+            </tr>
+        </table>
+    </section>
+    <aside>
+        <table>  
+            <tr>
+                <th>Participantes</th>
+            </tr>
+            <tr>
+                <td>Richard Alves👑</td>
+            </tr>
+            <tr>
+                <td>Jacira Fernandes</td>
+            </tr>
+            <tr>
+                <td>Cláudia Leitte</td>
+            </tr>
+            <tr>
+                <td>Edivaldo Rodrigues</td>
+            </tr>
+            <tr>
+                <td>Marcos Daré Silva Hess</td>
+            </tr>
+            <tr>
+                <td>Luis Azevedo Vitória</td>
+            </tr>
+        </table>
+    </aside>
+    <footer> 
+        <h2>Sua lista de desejos:</h2>
+        <table>  
+        <tr>
+            <td>Camiseta do Harry Potter</td>
+        </tr>
+        <tr>
+            <td>Capacete de moto</td>
+        </tr>
+        <tr>
+            <td>Um kit fronha e lençol  - cama de casal</td>
+        </tr>
+       </table>
+       <button class="bt1">Editar</button><button class="bt2">Salvar</button>
+    </footer>
+</body>
+</html>
